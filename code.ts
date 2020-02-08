2a0ecf6337e58e243444fc412a1ae793a431b15f
maxchehab <maxchehab@gmail.com>
2020-02-08 06:10:15 +0000

    fix

--- code.ts
+++ code.ts
@@ -1,13 +1,20 @@
 figma.showUI(__html__, { width: 300, height: 200 });
 
+const MAX_COLUMNS = 50;
+const MAX_ROWS = 50;
+
 figma.ui.onmessage = msg => {
   if (msg.type === "create-grid") {
-    const { rows, columns, rowPadding, columnPadding } = msg;
+    let { rows, columns, rowPadding, columnPadding } = msg;
+
+    rows = Math.min(rows, MAX_ROWS);
+    columns = Math.min(columns, MAX_COLUMNS);
 
     const selection = figma.currentPage.selection;
 
     if (!selection.length) {
       figma.closePlugin();
+      figma.notify("Please select either an object or group!");
       return;
     }
 
